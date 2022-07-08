38c8ddb1e0f1e6b4b7f61e47812289d84412d84f
soy hok <soyhok2018@gmail.com>
2022-07-08 15:17:29 +0700

    committed

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-// ask user to enter the start and the end number.
+start = int(input("Enter the start number: "))
+end = int(input("Enter the end number: "))
+for num in range(start, end +1):
+    if num > 1:
+        for i in range (2, num):
+            if (num%i) == 0:
+                break
+        else:
+            print(num)
+
