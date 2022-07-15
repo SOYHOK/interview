ee444444f1d9d52ab2f4dd869e7d4c16726cbc5a
soy hok <soyhok2018@gmail.com>
2022-07-15 14:49:58 +0700

    forth committed

---
+++ test3.py
@@ -0,0 +1,13 @@
+num = 123456
+test_num = 0
+while (num>0):
+    remainder = num % 10
+    test_num = remainder + (test_num*10)
+    num = num // 10
+print("Reverse is : {}".format(test_num))
+
+#2 mthod
+#num = input("Enter a number: ")
+#print('The reverse number is:')
+#for i in reversed(num):
+#   print(i, end='')
